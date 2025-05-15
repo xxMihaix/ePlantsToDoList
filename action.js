74a81099f1aa8da294fffabcb213e9c223d0e5aa
xxMihaix <marius777bogdan@gmail.com>
2025-05-15 17:02:32 +0300

    Add files via upload

--- action.js
+++ action.js
@@ -1,3 +1,81 @@
+
+
+let list = JSON.parse(localStorage.getItem("list")) || [];
+
+
+
+function renderList() {
+    const container = document.getElementById("second-container");
+    container.innerHTML = "";
+
+    for (let i = 0; i < list.length; i++) {
+        container.innerHTML += `
+        <div class="activity">
+            <p class="activ costum">${list[i].name}</p>
+            
+            <div class="day-hour-content">
+                <div class="day-hour">
+                    <p class="costum">Day:</p>
+                    <p class="costum">${list[i].day}</p>  
+                </div>
+
+                <div class="day-hour">
+                    <p class="costum">Hour:</p>
+                    <p class="costum">${list[i].hour}</p>
+                </div>
+            </div>
+
+            <button class="remove" onclick="removeElement()">Remove</button>
+        </div>`;
+    }
+}
+
+
+function removeElement () {
+    const button = document.querySelectorAll(".remove");
+
+    button.forEach((element, index) => {
+        element.addEventListener( 'click', function() {
+            list.splice(index, 1);
+            localStorage.setItem("list", JSON.stringify(list));
+            renderList();
+        })
+    })
+}
+
+
+function addList() {
+
+    const name = document.getElementById('activity-input');
+    const day = document.getElementById('day-input');
+    const hour = document.getElementById('hour-input');
+
+    let nameValue = name.value.trim();
+    let dayValue = day.value.trim();
+    let hourValue = hour.value.trim();
+
+    if(nameValue && dayValue && hourValue){
+        list.push({
+            name: nameValue,
+            day: dayValue, 
+            hour: hourValue
+        });
+        localStorage.setItem("list", JSON.stringify(list));
+    }
+
+    name.value = '';
+    day.value = '';
+    hour.value = '';
+
+    renderList();
+}
+
+window.onload = renderList;
+
+
+
+/*
+
 let list = JSON.parse(localStorage.getItem("list")) || [];
 
 function renderList() {
@@ -33,3 +111,4 @@ function removeItem(index) {
 }
 
 renderList();
+*/
